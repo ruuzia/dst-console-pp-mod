186ec348f8c2410db89691e8ed8cf59fb11603aa
RZia <36330543+grassdne@users.noreply.github.com>
2022-09-09 14:38:00 -0400

    don't autocomplete in strings or comments

--- main/completer.lua
+++ main/completer.lua
@@ -31,68 +31,77 @@ local function isindexable(v)
     return type(v) == "table" or type(GetMetaField(v, "__index")) == "table"
 end
 
-local function getpossiblekeys(str, search_start)
-    local tnames, calls = {}, {}
+local function findtable(str)
+    local search_start = str:find("[%w_]*$")
+    local indices = {}
+    -- Zero index is the one we're completing
+    indices[0] = { identifier = str:sub(search_start) }
     local expressionstart
-    local lastindexer = str:sub(search_start-1, search_start-1)
     -- We start at the end and go backwards matching indexing
     for wstart, word, call, indexer in keymatches_gen, str, search_start do
         expressionstart = wstart
-        table.insert(tnames, word)
+        local len = #indices
+        indices[len+1] = {
+            identifier = word,
+            call = call,
+            indexer = nil,
+        }
 
-        if call == "()" then
-            calls[#tnames] = "func"
-        elseif call ~= "" then
-            -- Invalid
-            return
-        end
-
-        if indexer == ":" then
-            -- t:func().
-            local lastcall = #tnames - 1
-            if calls[lastcall] == "func" then calls[lastcall] = "method" end
-        end
-        lastindexer = indexer
+        -- Set indexer of previous match
+        indices[len].indexer = indexer
     end
-    if #tnames <= 0 then return end
+
+    return indices, expressionstart
+end
+
+local function getpossiblekeys(indices)
+    -- Temp inject "ThePlayer" into console
+    local saved_ThePlayer = G.ThePlayer
+    G.ThePlayer = player
 
     local t = rawglobal
-    for i = #tnames, 1, -1 do
+    for i = #indices, 1, -1 do
+        local key = indices[i]
         local prevtbl = t
         -- Next layer in table
-        t = t[tnames[i]]
-        if calls[i] and iscallable(t) then
-            t = t(calls[i] == "method" and prevtbl or nil)
+        t = t[key.identifier]
+        if key.call == "()" and iscallable(t) then
+            t = t(key.indexer == ":" and prevtbl or nil)
         end
         if not isindexable(t) then return end
     end
+    -- Restore
+    G.ThePlayer = saved_ThePlayer
 
     local keys = {}
-    local onlyfuncs = str:byte(search_start - 1) == string.byte(':')
-    local tbls = {}
+    local onlyfuncs = indices[0].indexer == ":"
     -- For now I don't handle recursive __index chains
     local prevtbl
     for i,tbl in ipairs {t, GetMetaField(t, '__index')} do
         if type(tbl) == "table" then
             for k,v in pairs(tbl) do
-                if type(k) == "string" and (not onlyfuncs or iscallable(v)) and (not prevtbl or prevtbl[k] == nil) then
-                    -- Key is a string
-                    -- If `:`, value must be callable
-                    -- Key shouldn't be a duplicate
+                if type(k) == "string"
+                    and (not onlyfuncs or iscallable(v))    -- If `:`, value must be callable
+                    and (not prevtbl or prevtbl[k] == nil)  -- Key shouldn't be a duplicate
+                then
                     table.insert(keys, k)
                 end
             end
             prevtbl = tbl
         end
     end
+
     if #keys == 0 then return end
 
     local matches = {}
     local inds = {}
-    local search_string = str:sub(search_start)
-    if not Config.CASESENSITIVE then search_string = search_string:lower() end
+    local search_string = indices[0].identifier
+    --if not Config.CASESENSITIVE then search_string = search_string:lower() end
     for _, word in ipairs(keys) do
-        inds[word] = (Config.CASESENSITIVE and word or word:lower()):find(search_string, 1, true)
+        local cased_find = word:find(search_string, 1, true)
+        local insensitive_find = word:lower():find(search_string:lower(), 1, true)
+        -- Different casing works too, but it exact case match has higher precedence
+        inds[word] = cased_find or (insensitive_find and insensitive_find + 0.5) or nil
         if inds[word] then table.insert(matches, word) end
     end
 
@@ -101,11 +110,7 @@ local function getpossiblekeys(str, search_start)
     -- Sort first by start index and then alphabetically
     table.sort(matches, function(a, b) return inds[a] == inds[b] and a < b or inds[a] < inds[b] end)
 
-    return expressionstart, matches
-end
-
-local function getsearchstart(str)
-    return str:match("[.:]()[%w_]*$")
+    return matches
 end
 
 local function forcewordprediction(wp, str, exprstart, matches)
@@ -116,7 +121,7 @@ local function forcewordprediction(wp, str, exprstart, matches)
         GetDisplayString = simple_get_display_string,
         postfix = "",
     };
-    local search_start = getsearchstart(str)
+    local search_start = str:find("[%w_]*$")
     if search_start and #matches > 0 then
         wp.prediction = {
             start_pos = search_start-1,
@@ -128,21 +133,29 @@ local function forcewordprediction(wp, str, exprstart, matches)
     end
 end
 
-local function findindexing(text, cursorpos)
-    local _, endw = text:find("[%w_]*", cursorpos+1)
+local function find_word_end(text, cursorpos)
+    local _, endw = text:find("^[%w_]*", cursorpos+1)
     return endw or cursorpos
 end
 
 AddModRPCHandler(RPC_NAMESPACE, "RequestCompletions", function(player, str)
-    -- Temp inject "ThePlayer" into console
-    local saved_ThePlayer = G.ThePlayer
-    G.ThePlayer = player
-
-    local exprstart, matches = getpossiblekeys(str, getsearchstart(str))
+    local indices, exprstart = findtable(str)
+    local matches = getpossiblekeys(indices)
     if not matches then return end
     SendModRPCToClient(GetClientModRPC(RPC_NAMESPACE, "Completions"), player.userid, str, exprstart, table.concat(matches, '\n'))
+end)
 
-    G.ThePlayer = saved_ThePlayer
+AddModRPCHandler(RPC_NAMESPACE, "RequestGlobalCompletions", function(player, str)
+    local start = os.clock()
+    local search_string = str:match("[%w_]+$")
+    local matches = getpossiblekeys({ [0] = { identifier = search_string }, { identifier = "_G" } })
+    if matches then
+        SendModRPCToClient(GetClientModRPC(RPC_NAMESPACE, "Completions"),
+                           player.userid,
+                           str,
+                           #str - #search_string,
+                           table.concat(matches, '\n'))
+    end
 end)
 
 local _ignore = false
@@ -156,7 +169,7 @@ AddClientModRPCHandler(RPC_NAMESPACE, "Completions", function(completestr, exprs
     if not text:sub(1, #completestr) == completestr then return end
 
     matches = matches:split('\n')
-    local endpos = findindexing(text, #completestr)
+    local endpos = find_word_end(text, #completestr)
 
     local str = text:sub(1, endpos)
     local wp = scrn.console_edit.prediction_widget.word_predictor
@@ -168,38 +181,136 @@ AddClientModRPCHandler(RPC_NAMESPACE, "Completions", function(completestr, exprs
     scrn.console_edit.prediction_widget:RefreshPredictions()
 end)
 
-local _completionrequest_task
-function TryComplete(prediction_widget, text, pos, remote_execute)
+-- We don't want to be doing multiple server reqests in a frame
+local set_completion_request_task
+do
+    local _task
+    set_completion_request_task = function (callback)
+        local screen = TheFrontEnd:GetActiveScreen()
+        if screen.name == "ConsoleScreen" then
+            if _task then _task:Cancel() end
+            _task = screen.inst:DoTaskInTime(0, callback)
+        end
+    end
+end
+
+local function match_string_literal(str, i, delim)
+    assert(str:sub(i, i) == delim)
+    repeat
+        i = i + 1
+        local c = str:sub(i, i)
+        if c == '\\' then
+            -- Skip (escape) next character
+            i = i + 1
+        elseif c == delim then
+            return i
+        end
+    until i > #str
+    -- String didn't end
+end
+
+local function match_long_string_literal(str, i, delim)
+    local _, _end = str:find("%]"..delim.."%]", i)
+    return _end
+end
+
+local function long_literal_delim(str, i)
+    return str:match("%[(=*)%[", i)
+end
+
+local function match_comment(str, i)
+    assert(str:sub(i, i+1) == "--")
+    i = i + 2
+    if longcomment then
+        return match_long_string_literal(str, i, longcomment)
+    else
+        for i = 1, #str do
+            if str:sub(i, i) == '\n' then return i end
+        end
+    end
+end
+
+local function is_in_string_or_comment(str)
+    local i = 1
+    repeat
+        local c = str:sub(i, i)
+        if c == '\'' or c == '\"' then
+            i = match_string_literal(str, i, c)
+        elseif c == '-' then
+            if str:sub(i+1, i+1) == '-' then
+                i = match_comment(str, i)
+            end
+        else
+            local long = long_literal_delim(str, i)
+            if long then i = match_long_string_literal(str, i, long) end
+        end
+        if not i then return true end
+        i = i + 1
+    until i > #str
+    return false
+end
+
+local ignored_searches = {
+    ["do"] = true,
+    ["then"] = true,
+    ["end"] = true,
+}
+function TryComplete(prediction_widget, text, cursor_pos, remote_execute)
     local wp = prediction_widget.word_predictor
-    -- only doing this to force refresh the buttons
+    -- only being called to force refresh the buttons
     if _ignore then _ignore = false return true end
 
-    pos = findindexing(text, pos)
+    local pos = find_word_end(text, cursor_pos)
     local str = text:sub(1, pos)
-    local search_start = getsearchstart(str)
-    if not search_start then return false end
+    local search_start = str:find("[%w_]*$")
+    local running_in_client = modinfo.client_only_mod or IS_DEDICATED or not remote_execute
+    local indexer = str:sub(search_start-1, search_start-1)
+    if indexer == '.' or indexer == ':' then
+        if running_in_client then
+            local indices, exprstart = findtable(str)
+            local matches = getpossiblekeys(indices)
+            if not matches then
+                wp:Clear()
+                return true
+            end
+            forcewordprediction(wp, str, exprstart, matches)
+        else
+            set_completion_request_task(function()
+                SendModRPCToServer(GetModRPC(RPC_NAMESPACE, "RequestCompletions"), str)
+            end)
+        end
+
+    elseif search_start <= pos then
+        local search_string = text:sub(search_start)
+
+        if tonumber(search_string:sub(1, 1)) or ignored_searches[search_string] then
+            wp:Clear()
+            return true
+        elseif is_in_string_or_comment(str) then
+            -- Match prefab names
+            return false
+        end
+
+        if running_in_client then
+            local matches = getpossiblekeys({ [0] = { identifier = search_string }, { identifier = "_G" } })
+            if not matches then
+                wp:Clear()
+                return true
+            end
+            forcewordprediction(wp, str, search_start, matches)
+        else
+            -- We don't want to be doing multiple of these reqests in a frame
+            -- So each one overwrites the previous
+            set_completion_request_task(function()
+                SendModRPCToServer(GetModRPC(RPC_NAMESPACE, "RequestGlobalCompletions"), str)
+            end)
+        end
+
+    else return end
 
     wp.text = text
     wp.cursor_pos = pos
     --wp.prediction = nil
-
-    if modinfo.client_only_mod or IS_DEDICATED or not remote_execute then
-        local exprstart, matches = getpossiblekeys(str, search_start)
-        if not matches then
-            wp:Clear()
-            return true
-        end
-        forcewordprediction(wp, str, exprstart, matches)
-    else
-        -- We don't want to be doing multiple of these reqests in a frame
-        if _completionrequest_task then
-            _completionrequest_task:Cancel()
-            _completionrequest_task = nil
-        end
-        _completionrequest_task = prediction_widget.text_edit.inst:DoTaskInTime(0, function()
-            SendModRPCToServer(GetModRPC(RPC_NAMESPACE, "RequestCompletions"), str)
-        end)
-    end
 
     return true
 end
