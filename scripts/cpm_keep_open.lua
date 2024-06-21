06b2342c8a4122ba09b560e1d9267ff2b75771de
Rustum Zia <ziarustum@gmail.com>
2024-06-21 11:19:20 -0400

    [cpm_keep_open] fix and add test

--- scripts/cpm_keep_open.lua
+++ scripts/cpm_keep_open.lua
@@ -14,6 +14,8 @@ Hook(ConsoleScreen, "OnTextEntered", function (orig, screen, ...)
         G.ConsoleScreenSettings:Save()
         screen.console_edit:SetString("")
         screen.console_edit:SetEditing(true)
+        -- Go to end of history
+        screen.history_idx = nil
     else
         return orig(screen, ...)
     end
@@ -21,5 +23,27 @@ end)
 
 return {
     tests = {
+        -- Test for bug where history idx was off
+        ["test history updates properly on force run"] = function ()
+            Impurities:New(Config, "KEEPCONSOLEOPEN", true)
+            local screen = Tester.OpenConsole()
+            screen:ToggleRemoteExecute(true)
+
+            Tester.SendTextInput("--foo")
+            Tester.PressEnter()
+
+            Tester.SendKey(KEY_UP)
+            AssertEq(screen.console_edit:GetString(), "--foo")
+            Tester.SendTextInput("bar")
+            Tester.PressEnter()
+
+            Tester.SendKey(KEY_UP)
+            AssertEq(screen.console_edit:GetString(), "--foobar")
+            Tester.SendTextInput("bar")
+            Tester.PressEnter()
+            AssertEq(screen.console_edit:GetString(), "")
+--fooba--foobarr
+            Impurities:Restore(Config, "KEEPCONSOLEOPEN")
+        end,
     }
 }
