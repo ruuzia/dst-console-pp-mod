e5e8287ee80f5bbf4e88e806de56158229566942
Rustum Zia <ziarustum@gmail.com>
2024-06-23 18:49:43 -0400

    [text_navigation] better ctrl-left, ctrl-right, ctrl-backspace behaviour

--- scripts/consolepp/text_navigation.lua
+++ scripts/consolepp/text_navigation.lua
@@ -2,10 +2,15 @@ setfenv(1, ConsolePP.env)
 local G = GLOBAL
 local TextEdit = require "widgets/textedit"
 
+local function IsWordCharacter(c)
+    local ptrn = "["..Config.WORDSET.."]"
+    return c:find(ptrn) ~= nil
+end
+
 -- Changes for ALL textedits - not just console edit
 -- First OnRawKey to run
 Hook(TextEdit, "OnRawKey", function(orig, self, key, down)
-    local ctrl_down = TheInput:IsKeyDown(G.KEY_LCTRL) or TheInput:IsKeyDown(G.KEY_RCTRL)
+    local ctrl_down = TheInput:IsKeyDown(KEY_LCTRL) or TheInput:IsKeyDown(KEY_RCTRL) or TheInput:IsKeyDown(KEY_CTRL)
     local active_prediction_btn = self.prediction_widget and self.prediction_widget.active_prediction_btn
 
     if down then
@@ -20,12 +25,21 @@ Hook(TextEdit, "OnRawKey", function(orig, self, key, down)
         elseif key == KEY_BACKSPACE and ctrl_down then
             local str = self:GetString()
             local pos = self.inst.TextEditWidget:GetEditCursorPos()
-            if pos > 0 then
-                local ptrn = "["..Config.WORDSET.."]*[^"..Config.WORDSET.."]*$"
-                local i = str:sub(1, pos-1):find(ptrn)
-                self:SetString(str:sub(1, i-1) .. str:sub(pos + 1))
-                self.inst.TextEditWidget:SetEditCursorPos(i - 1)
+            local i = pos
+            if IsWordCharacter(str:sub(i, i)) then
+                -- move to start of word
+                while i > 0 and IsWordCharacter(str:sub(i, i)) do
+                    i = i - 1
+                end
+            else
+                -- move to end of previous word
+                while i > 0 and not IsWordCharacter(str:sub(i, i)) do
+                    i = i - 1
+                end
             end
+
+            self:SetString(str:sub(1, i) .. str:sub(pos + 1))
+            self.inst.TextEditWidget:SetEditCursorPos(i)
             return true
 
         elseif key == G.KEY_TAB then
@@ -36,7 +50,7 @@ Hook(TextEdit, "OnRawKey", function(orig, self, key, down)
             elseif Config.TABNEXT and active_prediction_btn then
                 local prediction_btns = self.prediction_widget.prediction_btns
                 if active_prediction_btn then
-                    if TheInput:IsKeyDown(G.KEY_LCTRL) or TheInput:IsKeyDown(G.KEY_RCTRL) then
+                    if ctrl_down then
                         prediction_btns[active_prediction_btn > 1 and active_prediction_btn - 1 or #prediction_btns]:Select()
                     else
                         prediction_btns[active_prediction_btn < #prediction_btns and active_prediction_btn + 1 or 1]:Select()
@@ -49,20 +63,36 @@ Hook(TextEdit, "OnRawKey", function(orig, self, key, down)
         elseif key == KEY_LEFT and ctrl_down then
             local str = self:GetString()
             local pos = self.inst.TextEditWidget:GetEditCursorPos()
-            if pos == 0 then return false end
-            local ptrn = "["..Config.WORDSET.."]*[^"..Config.WORDSET.."]*$"
-            local i = str:sub(1, pos):find(ptrn)
-            self.inst.TextEditWidget:SetEditCursorPos(i-1)
+            if IsWordCharacter(str:sub(pos, pos)) then
+                -- move to start of word
+                while pos > 0 and IsWordCharacter(str:sub(pos, pos)) do
+                    pos = pos - 1
+                end
+            else
+                -- move to end of previous word
+                while pos > 0 and not IsWordCharacter(str:sub(pos, pos)) do
+                    pos = pos - 1
+                end
+            end
+            self.inst.TextEditWidget:SetEditCursorPos(pos)
             ForceFocusTextEditCursor(self)
             return true
 
         elseif key == KEY_RIGHT and ctrl_down then
             local str = self:GetString()
             local pos = self.inst.TextEditWidget:GetEditCursorPos()
-            local ptrn = "^["..Config.WORDSET.."]*[^"..Config.WORDSET.."]*"
-            local _, i = str:find(ptrn, pos+1)
-            if not i then return false end
-            self.inst.TextEditWidget:SetEditCursorPos(i)
+            if pos <  #str and IsWordCharacter(str:sub(pos+1, pos+1)) then
+                -- move to end of word
+                while IsWordCharacter(str:sub(pos+1, pos+1)) do
+                    pos = pos + 1
+                end
+            else
+                -- move to start of next word
+                while pos < #str and not IsWordCharacter(str:sub(pos+1, pos+1)) do
+                    pos = pos + 1
+                end
+            end
+            self.inst.TextEditWidget:SetEditCursorPos(pos)
             ForceFocusTextEditCursor(self)
             return true
 
@@ -117,11 +147,23 @@ return {
             temp:Purge()
         end,
 
-        -- ["test ctrl-left and ctrl-right"] = function ()
-        --     local screen = Tester.OpenConsole()
-        --     Tester.SendTextInput("foo().components.bar .baz = 42")
-        --     Tester.WithKeysDown({ KEY_CTRL }, Tester.SendKey, KEY_LEFT)
-        --     Tester.WithKeysDown({ KEY_CTRL }, Tester.SendKey, KEY_LEFT)
-        -- end,
+        ["test word movements"] = function ()
+            local screen = Tester.OpenConsole()
+            Tester.SendTextInput("foo().components.bar!@#!@$baz = 42")
+            Tester.WithKeysDown({ KEY_CTRL }, function ()
+                Tester.SendKey(KEY_LEFT)
+                Tester.SendKey(KEY_LEFT)
+                Tester.SendKey(KEY_LEFT)
+                Tester.SendKey(KEY_BACKSPACE)
+            end)
+            Tester.SendTextInput(".")
+            Tester.WithKeysDown({ KEY_CTRL }, function ()
+                Tester.SendKey(KEY_RIGHT)
+                Tester.SendKey(KEY_RIGHT)
+                Tester.SendKey(KEY_RIGHT)
+            end)
+            AssertEq(screen.console_edit:GetString(), "foo().components.bar.baz = 42")
+            AssertEq(screen.console_edit.inst.TextEditWidget:GetEditCursorPos(), #"foo().components.bar.baz = 42")
+        end,
     }
 }
