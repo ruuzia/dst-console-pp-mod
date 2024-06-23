743a3dc8a19768ce300e18f521b056acfc149f01
Rustum Zia <ziarustum@gmail.com>
2024-06-22 23:10:49 -0400

    [completion_key_config] MORE TESTS MWAHAHAHA

--- scripts/consolepp/completion_key_config.lua
+++ scripts/consolepp/completion_key_config.lua
@@ -12,4 +12,55 @@ Hook(WordPredictionWidget, "_ctor", function(orig, self, ...)
 end)
 
 
-return {}
+return {
+    tests = {
+        ["test Config.ENTERCOMPLETE=true"] = function ()
+            local temp = State()
+            temp:Set(Config, "ENTERCOMPLETE", true)
+
+            local screen = Tester.OpenConsole()
+            Tester.SendTextInput("c_boatcollisi")
+            Tester.PressEnter()
+            AssertEq(screen.console_edit:GetString(), "c_boatcollision")
+
+            temp:Purge()
+        end,
+        ["test Config.ENTERCOMPLETE=false"] = function ()
+            local temp = State()
+            temp:Set(Config, "ENTERCOMPLETE", false)
+            temp:Set(Config, "KEEPCONSOLEOPEN", true)
+
+            local screen = Tester.OpenConsole()
+            Tester.SendTextInput("c_boatcollisi")
+            Tester.PressEnter()
+            AssertEq(screen.console_edit:GetString(), "")
+
+            temp:Purge()
+        end,
+        ["test Config.TABCOMPLETE=true"] = function ()
+            local temp = State()
+            temp:Set(Config, "TABCOMPLETE", true)
+
+            local screen = Tester.OpenConsole()
+            Tester.SendTextInput("c_boatcollisi")
+            Tester.SendKey(KEY_TAB)
+            AssertEq(screen.console_edit:GetString(), "c_boatcollision")
+
+            temp:Purge()
+        end,
+        ["test Config.TABCOMPLETE=false"] = function ()
+            local temp = State()
+            temp:Set(Config, "TABCOMPLETE", false)
+            temp:Set(Config, "TABINSERT", true)
+
+            local screen = Tester.OpenConsole()
+            Tester.SendTextInput("c_boatcollisi")
+            Tester.SendKey(KEY_TAB)
+            -- Note: we don't want to insert spaces here either,
+            -- because who wants tabs in the middle of a line?
+            AssertEq(screen.console_edit:GetString(), "c_boatcollisi")
+
+            temp:Purge()
+        end,
+    }
+}
