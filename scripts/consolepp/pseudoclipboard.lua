2dea1b36dc6403ebda128497d2cead6445325a8e
Rustum Zia <ziarustum@gmail.com>
2024-06-29 16:52:23 -0400

    [pseudoclipboard] make clipboard work two-ways

--- scripts/consolepp/pseudoclipboard.lua
+++ scripts/consolepp/pseudoclipboard.lua
@@ -1,50 +1,136 @@
--- We can't write to the clipboard, and this isn't a particularly
--- convenient solution. But it's a temporary fix so there's at least
--- *some* way to save the contents of console log.
+-- We can't write to the clipboard, which makes things inconvenient.
+-- This module provides a solution to save the contents of the console to a file which can then be retrieved.
+-- However, the idea of this module is to make it easy to switch between writing a command from the game
+-- console and editing a command from within a real text editor.
+-- Now, I've figured out how to make the game let me save files right to the local mods folder,
+-- which is a much more convenient location to access.
 --
--- Use Ctrl+C to write to psuedoclipboard and Ctrl+Shift+C to replace
--- current console contents with content of psuedoclipboard.
+-- Use Ctrl+C to write to pseudoclipboard and Ctrl+Shift+C to replace
+-- current console contents with content of pseudoclipboard.
 
 setfenv(1, ConsolePP.env)
 local G = GLOBAL
 local ConsoleScreen = require "screens/consolescreen"
 
--- Remove old typo file --
-AddGamePostInit(function() TheSim:ErasePersistentString("../psuedoclipboard.txt") end)
-------------------------------
-local PSEUDO_CLIPBOARD_FILE = "../pseudoclipboard.txt"
+local INFO_TEXT = [[
 
--- Keep CTRL from toggling remote execute if you press enter
+--------------------------------------
+----- Console++ Pseudo-Clipboard -----
+--------------------------------------
+-- This file is to make it easier to switch between editing a command within
+-- the game and editing it in a real text editor. From the console, use Ctrl+C
+-- to replace the file's content with the current console input. To send the
+-- file content back to the console, use Ctrl+Shift+C.
+-- Lines commented out with "--" will be left.
+]]
+
+-- Remove old files --
+AddGamePostInit(function() TheSim:ErasePersistentString("../psuedoclipboard.txt") end)
+AddGamePostInit(function() TheSim:ErasePersistentString("../pseudoclipboard.txt") end)
+------------------------------
+
+local DIRECTORY_PATH = "../mods/"
+local DATA_FILE_PATH = DIRECTORY_PATH.."console_clipbaord.lua"
+
+-- Make game allow me to open this directory
+do
+    local found = false
+    for _, item in pairs(G.package.assetpath) do
+        if item.path == DIRECTORY_PATH then
+            found = true
+            break
+        end
+    end
+    if not found then
+        table.insert(G.package.assetpath, { path = DIRECTORY_PATH })
+    end
+end
+
+local function GetClipboardFileLines()
+    local file, err = G.io.open(DATA_FILE_PATH, "r")
+    if not file then
+        Log("Error: could not read from %s: %s", DATA_FILE_PATH, err)
+        return
+    end
+    local lines = {}
+    for line in file:lines() do
+        table.insert(lines, line)
+    end
+    file:close()
+    return lines
+end
+
+local function ShouldIgnoreLine(line)
+    return string.starts(line, "--")
+end
+
+local function PushClipboard(contents)
+    local file_lines = GetClipboardFileLines() or {}
+    local lines = {}
+    for line in contents:gmatch("([^\n]+)") do
+        table.insert(lines, line)
+    end
+    for _,line in ipairs(file_lines) do
+        if ShouldIgnoreLine(line) then
+            table.insert(lines, line)
+        end
+    end
+
+    local file, err = G.io.open(DATA_FILE_PATH, "w")
+    if not file then
+        Log("Error: could not write to %s: %s", DATA_FILE_PATH, err)
+        return
+    end
+    file:write(table.concat(lines, "\n"))
+    file:close()
+    Log("Console contents saved to %s", DATA_FILE_PATH)
+end
+
+local function PullClipboard()
+    local file_lines = GetClipboardFileLines()
+    if file_lines then
+        local lines = {}
+        for _, line in ipairs(file_lines) do
+            if not ShouldIgnoreLine(line) then
+                table.insert(lines, line)
+            end
+        end
+        Log("Console contents pulled from %s", DATA_FILE_PATH)
+        return table.concat(lines, "\n")
+    end
+end
+
 Hook(ConsoleScreen, "OnRawKeyHandler", function (orig, screen, key, down, ...)
     if key == KEY_C and down and TheInput:IsKeyDown(KEY_CTRL)
         and not TheInput:IsKeyDown(KEY_ALT) -- In case keyboard layouts that use C
     then
         if TheInput:IsKeyDown(KEY_SHIFT) then
-            TheSim:GetPersistentString(PSEUDO_CLIPBOARD_FILE, function (succ, content)
-                if succ then
-                    screen.console_edit:SetString(content)
-                    Log("Console contents pulled from "..PSEUDO_CLIPBOARD_FILE.."!")
-                else
-                    Log("Erorr: Unable to pull console contents from "..PSEUDO_CLIPBOARD_FILE"..!")
-                end
-            end)
+            local content = PullClipboard()
+            if content then
+                screen.console_edit:SetString(content)
+            end
         else
-            TheSim:SetPersistentString(PSEUDO_CLIPBOARD_FILE, screen.console_edit:GetString(), false, function (succ)
-                if succ then
-                    Log("Console contents saved to "..PSEUDO_CLIPBOARD_FILE.."!")
-                else
-                    Log("Erorr: Unable to write console contents to "..PSEUDO_CLIPBOARD_FILE"..!")
-                end
-            end)
+            PushClipboard(screen.console_edit:GetString())
         end
     end
     return orig(screen, key, down, ...)
 end)
 
--- Oh and we need to do this too
 Hook(ConsoleScreen, "_ctor", function (constructor, screen, ...)
     constructor(screen, ...)
 	screen.console_edit.validrawkeys[KEY_C] = true
+
+    -- Initialize file
+    if not G.kleifileexists(DATA_FILE_PATH) then
+        local f = G.io.open(DATA_FILE_PATH, "w")
+        if f then
+            Log("Successfully initialized %s", DATA_FILE_PATH)
+            f:write(INFO_TEXT)
+            f:close()
+        else
+            Log("Error: failed to open %s", DATA_FILE_PATH)
+        end
+    end
 end)
 
 return {}
